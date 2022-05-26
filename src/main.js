040d4c48b096423ad44ceed961aaa664dcc78603
Ahmetzt <ahmet@bcworks.com.tr>
2022-05-26 11:43:13 +0300

    Publish 220526

--- src/main.js
+++ src/main.js
@@ -25,7 +25,7 @@ Vue.use(IconsPlugin)
 // Vue.use(FormDatepickerPlugin)
 
 Axios.defaults.baseURL = "http://192.168.10.71/HelisAppWebAPI/api"
-// Axios.defaults.baseURL = "http://testwebapi.helisapp.com/api"
+// Axios.defaults.baseURL = "https://testwebapi.helisapp.com/api"
 // Axios.defaults.baseURL = "http://192.168.1.63/HelisAppWebAPI/api"
 // Axios.defaults.baseURL = "http://192.168.1.39/HelisAppWebAPI/api"
 
